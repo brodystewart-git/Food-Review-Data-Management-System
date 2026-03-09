3af7969b9c16a701b6eed7b78ddddcde11e67515
Brody Stewart <87514977+brodystewart-git@users.noreply.github.com>
2026-03-08 23:06:22 -0400

    Create Category.java v0.25
    Category for phase 1.

---
+++ java/src/Category.java
@@ -0,0 +1,20 @@
+/*
+ * Brody Stewart
+ * CEN 3024 - Software Development 1
+ * March 8th, 2026
+ * Category.java
+ * This application contains the Category enumeration.
+ * Holding this information as an enumerable data type
+ * makes it easier to remove or add new categories later in development.
+ */
+public enum Category{
+    FRUIT,
+    VEGETABLE,
+    DAIRY,
+    GRAIN,
+    MEAT,
+    SEAFOOD,
+    BEVERAGE,
+    SUGARS,
+    OTHER
+}
