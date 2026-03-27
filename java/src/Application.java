58b433463e7b23243b975a727b90b2a6d0725aa7
Brody Stewart <87514977+brodystewart-git@users.noreply.github.com>
2026-03-26 20:10:33 -0400

    Refactor Application class to simplify main method

--- java/src/Application.java
+++ java/src/Application.java
@@ -1,7 +1,7 @@
 /*
  * Brody Stewart
  * CEN 3024 - Software Development 1
- * March 8th, 2026
+ * March 26th, 2026
  * Application.java
  * This application launches the DMS Review System program, currently done this way for Phase 1 of the project.
  * It does this by calling the actual program's system class, the Review System.
@@ -16,15 +16,6 @@ public class Application {
      */
     public static void main(String[] args) {
         ReviewSystem sys = new ReviewSystem();
-        System.out.println("Welcome!\n\n");
-        int load = sys.loadFile();
-        //Load returns -1 if the user types exit.
-        if (load != -1) {
-            boolean running = true;
-            while(running) {
-                running = sys.initGui();
-            }
-        }
-        System.out.println("Have a good day.");
+        sys.initGui();
     }
 }
