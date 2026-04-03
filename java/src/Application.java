bd6eb660f37335ad069026939ebaffaa097023a4
Brody Stewart <87514977+brodystewart-git@users.noreply.github.com>
2026-04-03 14:41:40 -0400

    Refactor Application class and update documentation
    Updated comments and removed GUI initialization in main method.

--- java/src/Application.java
+++ java/src/Application.java
@@ -1,21 +1,13 @@
 /*
  * Brody Stewart
  * CEN 3024 - Software Development 1
- * March 26th, 2026
+ * April 3rd, 2026
  * Application.java
- * This application launches the DMS Review System program, currently done this way for Phase 1 of the project.
- * It does this by calling the actual program's system class, the Review System.
+ * This application launches the DMS Review System program through the Review System object.
  * Usage of the program will be explained in the comments of that file.
  */
 public class Application {
-
-    /*
-    * This is the main and only function of this file.
-    * It sets up the program by getting a file to save a read from.
-    * Then, it runs the program until the user exits in some way.
-     */
     public static void main(String[] args) {
         ReviewSystem sys = new ReviewSystem();
-        sys.initGui();
     }
 }
